87c90b44fb2ef1d0fccdd9526540958e45049ca6
ShengUei <97223740+ShengUei@users.noreply.github.com>
2022-08-26 15:01:51 +0800

    Update logging.py

--- logging/logging.py
+++ logging/logging.py
@@ -1,3 +1,30 @@
 from datetime import datetime, timezone
 import logging
-
+import os
+ 
+dir_path = 'D:/pythonProject/itkm/logger/logs/'   # 設定 logs 目錄
+filename = "{:%Y-%m-%d}".format(datetime.now()) + '.log'     # 設定檔名
+ 
+def create_logger(log_folder):
+    # config
+    logging.captureWarnings(True)   # 捕捉 py waring message
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    my_logger = logging.getLogger('py.warnings')    # 捕捉 py waring message
+    my_logger.setLevel(logging.INFO)
+ 
+    # 若不存在目錄則新建
+    if not os.path.exists(dir_path+log_folder):
+        os.makedirs(dir_path+log_folder)
+ 
+    # file handler
+    fileHandler = logging.FileHandler(dir_path+log_folder+'/'+filename, 'w', 'utf-8')
+    fileHandler.setFormatter(formatter)
+    my_logger.addHandler(fileHandler)
+ 
+    # console handler
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setLevel(logging.DEBUG)
+    consoleHandler.setFormatter(formatter)
+    my_logger.addHandler(consoleHandler)
+ 
+    return my_logger
