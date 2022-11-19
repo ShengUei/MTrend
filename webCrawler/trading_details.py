e11c9d3264d9973a91505032993363fc97c8caaa
ShengUei <shengueiwengtw@gmail.com>
2022-11-20 01:21:37 +0800

    trading detail

--- webCrawler/trading_details.py
+++ webCrawler/trading_details.py
@@ -4,6 +4,25 @@ from util.request import get_request
 from logger.logger import get_logger, close_handler
 
 def get_daily_trading_details():
+    selectType = {
+                    'Cement': '01', 'Food': '02', 'Plastic': '03', 'Textile': '04', 'Electric, Machinery': '05', 
+                    'Electrical and Cable': '06', 'Chemical, Biotechnology, and Medical Care Industry': '07', 'Glass and Ceramic': '08', 'Paper and Pulp': '09', 'Iron and Steel': '10', 
+                    'Rubber': '11', 'Automobile': '12', 'Electronic': '13', 'Building Material and Construction': '14', 'Shipping and Transportation': '15', 
+                    'Tourism': '16', 'Financial and Insurance': '17', 'Trading and Consumers Goods Industry': '18', 'General': '19', 'Other Industry': '20', 
+                    'Chemical Industry': '21', 'Biotechnology and Medical Care Industry': '22', 'Oil, Gas and Electricity Industry': '23', 'Semiconductor Industry': '24', 'Computer and Peripheral Equipment Industry': '25', 
+                    'Computer and Peripheral Equipment Industry': '26', 'Communications and Internet Industry': '27', 'Electronic Parts/Components Industry': '28', 'Electronic Products Distribution Industry': '29', 'Information Service Industry': '30',
+                    'Information Service Industry': '31'
+                }
+    
+    try:
+        for type in selectType.values():
+            get_trading_details(type)
+
+    except Exception as e:
+        print("Excetion $s", e)
+
+
+def get_trading_details(self, selectType: str):
     logger = get_logger()
 
     try:
@@ -13,8 +32,7 @@ def get_daily_trading_details():
         date_str = local_datetime.strftime('%Y%m%d')
 
         HOST = 'www.twse.com.tw'
-        selectType = ['ALL', 24] #24: 半導體業
-        url = 'https://www.twse.com.tw/en/fund/T86?response=json&date={0}&selectType={1}'.format(date_str, selectType[1])
+        url = 'https://www.twse.com.tw/en/fund/T86?response=json&date={0}&selectType={1}'.format(date_str, selectType)
         stat_code = {0: 'OK', 1: 'No Data!'}
 
         if(local_weekday > 5):
@@ -42,4 +60,4 @@ def get_daily_trading_details():
     
     finally:
         close_handler(logger)
-
+        
